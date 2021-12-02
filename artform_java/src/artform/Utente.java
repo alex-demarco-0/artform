487cb718a878a1476063a4b26f7a9726d366db4e
Alessandro De Marco <a.demarco@itsrizzoli.it>
2021-12-02 10:27:44 +0100

    file java

---
+++ artform_java/src/artform/Utente.java
@@ -0,0 +1,13 @@
+package artform;
+
+public class Utente {
+	
+	private static int nextUtente = 1;
+	private final int ID;
+	private String nome, cognome, username, email, numeroTelefono, password;
+	
+	public Utente(String n, String c, String u) {
+		
+	}
+	
+}
