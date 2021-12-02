afb2bff3ea36130c869787b80a332645a4c8c7d8
Alessandro De Marco <a.demarco@itsrizzoli.it>
2021-12-02 10:40:52 +0100

    upd

--- artform_java/src/artform/Utente.java
+++ artform_java/src/artform/Utente.java
@@ -2,18 +2,18 @@ package artform;
 
 public class Utente {
 	
-	private static int nextUtente = 1;
+	//private static int nextUtente = 1;
 	private final int ID;
 	private String nome, cognome, username, email, numeroTelefono, password;
 	
-	public Utente(String nome, String cognome, String username, String numeroTelefono, String password) {
-		ID = nextUtente;
+	public Utente(int ID, String nome, String cognome, String username, String numeroTelefono, String password) {
+		this.ID = ID;
 		this.nome = nome;
 		this.cognome = cognome;
 		this.username = username;
 		this.numeroTelefono = numeroTelefono;
 		this.password = password;
-		nextUtente++;
+		//nextUtente++;
 	}
 	public String toString(){
 		return "Ciao " + nome + " " + cognome + " detto anche " + username;
