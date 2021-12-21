3be56950c6273c89748d4b86871858c018aab4e4
ManbirSinghB <s.manbir431@gmail.com>
2021-12-21 16:52:57 +0100

    navigation  bar

--- artform_android/app/src/main/java/it/artform/MainActivity.java
+++ artform_android/app/src/main/java/it/artform/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         bottomNavigation;
+        bottomNavigation.selectedItemId = R.id.page_2
     }
+
+
 }
