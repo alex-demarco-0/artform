cae4ee6564b1d6261a5e83277938736f4a4c0bfe
Alessandro De Marco <a.demarco@itsrizzoli.it>
2021-12-21 16:53:03 +0100

    wip main activity layout

--- artform_android/app/src/main/java/it/artform/MainActivity.java
+++ artform_android/app/src/main/java/it/artform/MainActivity.java
@@ -2,7 +2,9 @@ package it.artform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pubblica(View view) {
+        Intent pub = new Intent(this, ContentPubActivity.class);
+        startActivity(pub);
     }
 }
--- artform_android/app/src/main/java/it/artform/RegisterActivity.java
+++ artform_android/app/src/main/java/it/artform/RegisterActivity.java
@@ -2,7 +2,9 @@ package it.artform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void registrazione(View view) {
+        Intent reg = new Intent(this, MainActivity.class);
+        startActivity(reg);
     }
 }
