a3633a226dc71d9c313671ba2cb2114b633deef2
Arianna Dituri <a.dituri@itsrizzoli.it>
2021-12-21 15:53:43 +0100

    User Profile

--- artform_android/app/src/main/java/it/artform/UserProfileActivity.java
+++ artform_android/app/src/main/java/it/artform/UserProfileActivity.java
@@ -1,8 +1,13 @@
 package it.artform;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_user_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        int itemId=item.getItemId();
+        switch(itemId){
+            case R.id.SETTINGS:
+                Intent profileSettings=new Intent(this, profileSettings.class);
+                startActivity(profileSettings);
+                break;
+            case R.id.SAVED:
+                Intent savedPost=new Intent(this, savedPost.class);
+                startActivity(savedPost);
+                break;
+        }
     }
 }
