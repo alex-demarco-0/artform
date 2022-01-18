813bd400509e93cf63fe2362437c5e0bdf37aa4c
ManbirSinghB <s.manbir431@gmail.com>
2022-01-18 09:56:04 +0100

    controllo password in LoginActivity.java

--- artform_android/app/src/main/java/it/artform/LoginActivity.java
+++ artform_android/app/src/main/java/it/artform/LoginActivity.java
@@ -3,8 +3,10 @@ package it.artform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,7 +16,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         EditText loginUsername = findViewById(R.id.loginUsername);
-        //Button loginButton = findViewById(R.id.loginButton);
+        EditText loginPassword = findViewById(R.id.loginPassword);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (loginUsername.getText().toString().equals("admin") && loginPassword.getText().toString().equals("admin")){
+                    Toast.makeText(LoginActivity.this, "Login effetutato", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Credenziali errate", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
 
     }
 }
