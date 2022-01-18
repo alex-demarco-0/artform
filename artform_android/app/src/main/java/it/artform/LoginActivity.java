8c443b5fcb6ea3ede04185cd3908eb638535f894
ariannadituri <94626467+ariannadituri@users.noreply.github.com>
2022-01-18 10:02:43 +0100

    LoginActivity intent->RegisterActivity
    Non so se il mio codice è corretto, ho fatto una prova

--- artform_android/app/src/main/java/it/artform/LoginActivity.java
+++ artform_android/app/src/main/java/it/artform/LoginActivity.java
@@ -2,9 +2,12 @@ package it.artform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,15 +16,38 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        //istanziamento campi edit e button
         EditText loginUsername = findViewById(R.id.loginUsername);
-        HEAD
         Button loginButtonAccedi = findViewById(R.id.loginButtonAccedi);
         Button loginButtonRegistrati = findViewById(R.id.loginButtonRegistrati);
         EditText loginPassword = findViewById(R.id.loginPassword);
 
 
 
-        //Button loginButton = findViewById(R.id.loginButton);
+        loginButtonAccedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Vai alla home page
+                //Toast di prova
+                Toast.makeText(LoginActivity.this, "Hai premuto ACCEDI", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        loginButtonRegistrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast di prova
+                Toast.makeText(LoginActivity.this, "Hai premuto REGISTRATI", Toast.LENGTH_LONG).show();
+                //Vai alla RegisterActivity
+                Intent openRegisterActivity= new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(openRegisterActivity);
+            }
+
+
+        });
+
+
 
 
     }
