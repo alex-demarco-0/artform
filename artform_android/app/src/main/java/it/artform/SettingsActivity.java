cdf6d805ce5b235f465529635e052757cb4f5367
ManbirSinghB <s.manbir431@gmail.com>
2022-01-24 18:00:19 +0100

    activity setting

--- artform_android/app/src/main/java/it/artform/SettingsActivity.java
+++ artform_android/app/src/main/java/it/artform/SettingsActivity.java
@@ -3,15 +3,29 @@ package it.artform;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class SettingsActivity extends Activity {
+import java.util.ArrayList;
 
+public class SettingsActivity extends Activity {
+    ListView settingsListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
+        setContentView(R.layout.activity_settings);
+        //ListView dei settings
+        settingsListView = (ListView) findViewById(R.id.settingsListView);
+        // inizializzazione
+        ArrayList<String> arrayList= new ArrayList<>();
+        arrayList.add("Saved post");
+        arrayList.add("Account");
+        arrayList.add("Privacy");
+        arrayList.add("Sicurezza");
+        arrayList.add("Assistenza");
+        arrayList.add("Informazioni");
 
         TextView logoutTextView = findViewById(R.id.logoutTextView);
         logoutTextView.setOnClickListener(new View.OnClickListener() {
