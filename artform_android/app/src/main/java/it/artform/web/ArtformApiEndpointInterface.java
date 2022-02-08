d093ad031a6fcc305ab0aec9afda87659817ddc7
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-02-08 15:47:29 +0100

    Test registrazione utente, modifiche all'ApiEndpointInterface

--- artform_android/app/src/main/java/it/artform/web/ArtformApiEndpointInterface.java
+++ artform_android/app/src/main/java/it/artform/web/ArtformApiEndpointInterface.java
@@ -10,19 +10,18 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ArtformApiEndpointInterface {
-    //current address: 10.0.2.2
-    String addr = "10.0.2.2";
+    //current address: http://10.0.2.2:8080/artform
 
-    @GET("http://" + addr + ":8080/artform/utente/{username}")
+    @GET("/utente/{username}")
     Call<User> getUserByUsername(@Path("username") String username);
 
-    @POST("http://xxx:8080/artform/utente")
+    @POST("/utente")
     Call<User> addUser(@Body User user);
 
-    @PUT("http://xxx:8080/artform/utente/{username}")
+    @PUT("/utente/{username}")
     Call<User> updateUser(@Path("username") String username, @Body User user);
 
-    @DELETE("http://xxx:8080/artform/utente/{username}")
+    @DELETE("/utente/{username}")
     String deleteUser(@Path("username") String username);
 
 }
