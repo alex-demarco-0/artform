572a24b4cec7b9a02d6a87f6eb3d6fa1429d4d15
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-02-08 15:54:09 +0100

    classe Application AFGlobal, dichiarazione nel manifest

--- artform_android/app/src/main/java/it/artform/web/AFGlobal.java
+++ artform_android/app/src/main/java/it/artform/web/AFGlobal.java
@@ -1,15 +1,15 @@
-package it.artform.databases;
+package it.artform.web;
 
 import android.app.Application;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class AFRetrofit extends Application {
-    public static final String BASE_URL = "http://10.0.2.2:8080/artform/utente";
+public class AFGlobal extends Application {
+    public static final String BASE_URL = "http://10.0.2.2:8080/artform";
     Retrofit retrofit = null;
 
-    public AFRetrofit() {
+    public AFGlobal() {
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
