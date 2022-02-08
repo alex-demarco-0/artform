a58f0a94d638bb8eb8befc2e6f41adabf6323650
ManbirSinghB <s.manbir431@gmail.com>
2022-02-08 14:41:04 +0100

    aggiunta @RestController

--- artform_springboot/src/main/java/it/artform/web/ArtformRESTController.java
+++ artform_springboot/src/main/java/it/artform/web/ArtformRESTController.java
@@ -2,6 +2,7 @@ package it.artform.web;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.artform.database.ArtformRepository;
 import it.artform.pojos.*;
 
+@RestController
 public class ArtformRESTController {
 	
 	@Autowired
