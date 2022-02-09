a332f856ddcd940698a61c56daeac9c4389dbc2d
ariannadituri <94626467+ariannadituri@users.noreply.github.com>
2022-02-09 14:23:00 +0100

    Gestione pulsante contactMe

--- artform_android/app/src/main/java/it/artform/ExternalProfileActivity.java
+++ artform_android/app/src/main/java/it/artform/ExternalProfileActivity.java
@@ -2,7 +2,9 @@ package it.artform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,6 +28,17 @@ public class ExternalProfileActivity extends AppCompatActivity {
         notifyMeExternalProfile=findViewById(R.id.nofifyMeExternalProfile);
         badgeExternalProfile=findViewById(R.id.badgeButtonExternalProfile);
         contactMe=findViewById(R.id.contactmeButton);
+
+        Button contactMe=(Button) findViewById(R.id.contactmeButton);
+        contactMe.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent=new Intent(ExternalProfileActivity.this, CommissionActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
 
     }
 }
