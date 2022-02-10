f27aeaa3e9303f47d0685e2d8b3023e9c0b8f1ef
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-02-10 16:21:25 +0100

    proseguimento Login e Registrazione

--- artform_android/app/src/main/java/it/artform/web/UserGetCallback.java
+++ artform_android/app/src/main/java/it/artform/web/UserGetCallback.java
@@ -1,5 +1,8 @@
 package it.artform.web;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import it.artform.pojos.User;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -7,11 +10,20 @@ import retrofit2.Response;
 
 public class UserGetCallback implements Callback<User> {
     private User user = null;
+    Context ctx = null;
+
+    public UserGetCallback(Context c) {
+        super();
+        ctx = c;
+    }
 
     @Override
     public void onResponse(Call<User> call, Response<User> response) {
-        if(response.code() == 200)
-            user = response.body();
+        if(response.isSuccessful()) {
+            user = new User(response.body().getName(), response.body().getSurname(), response.body().getUsername(),
+                    response.body().getEmail(), response.body().getPhone(), response.body().getPassword(), response.body().getPoints());
+            Toast.makeText(ctx, user.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
