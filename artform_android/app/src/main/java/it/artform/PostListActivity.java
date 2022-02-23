760c45bc57db794e8924a2ccf771847ae687032e
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-02-23 17:23:56 +0100

    creata activity per la lista dettagliata dei post di un utente

---
+++ artform_android/app/src/main/java/it/artform/PostListActivity.java
@@ -0,0 +1,13 @@
+package it.artform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_list);
+    }
+}
