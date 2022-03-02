db827fcc8946b8e18eba87fabcf4773194cef86e
ManbirSinghB <s.manbir431@gmail.com>
2022-03-02 12:04:09 +0100

    Aggiunta TopicSelectActivity, per selezionare i topic nella fase di registrazione e modifiche nei file xml

---
+++ artform_android/app/src/main/java/it/artform/TopicView.java
@@ -0,0 +1,26 @@
+package it.artform;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+public class TopicView extends FrameLayout {
+    TextView textView;
+    public TopicView(@NonNull Context context) {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.row_grid_item, this);
+        textView = (TextView) getRootView().findViewById(R.id.topicGridName);
+    }
+
+    public void display(String text, boolean isSelected){
+        textView.setText(text);
+        display(isSelected);
+    }
+
+    private void display(boolean isSelected) {
+    }
+
+}
---
+++ artform_android/app/src/main/java/it/artform/feed/TopicGridAdapter.java
@@ -0,0 +1,99 @@
+package it.artform.feed;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.artform.R;
+import it.artform.TopicView;
+import it.artform.pojos.Topic;
+
+public class TopicGridAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private String[] topicStrig;
+    public  List selectedPositions;
+
+    public TopicGridAdapter(Activity activity, String[] topicStrig) {
+        this.activity = activity;
+        this.topicStrig = topicStrig;
+        selectedPositions = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return topicStrig.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return topicStrig[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TopicView topicView = (view == null) ? new TopicView(activity) : (TopicView) view;
+        topicView.display(topicStrig[i], selectedPositions.contains(i));
+        return topicView;
+    }
+    /*
+    private Context ctx;
+    private List<Topic> topics;
+    private LayoutInflater layoutInflater;
+
+    public TopicGridAdapter(Context context, List<Topic> objects){
+        ctx = context;
+        topics = objects;
+    }
+
+    @Override
+    public int getCount() {
+        return topics.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        return null;
+    }
+
+//    @Override
+//    public View getView(int i, View view, ViewGroup viewGroup) {
+//        if (layoutInflater == null)
+//            layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//        if (view == null){
+//            view = layoutInflater.inflate(R.layout.row_grid_item, null);
+//        }
+//
+//        TextView topicGridName = view.findViewById(R.id.topicGridName);
+//        topicGridName.setText(topics.get(i).getName());
+//
+//        return view;
+//    }
+
+     */
+}
