7e6046529594cec1983cd9fdfaf84093128b74a2
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-03-02 12:57:25 +0100

    wip ExternalProfileActivity

--- artform_android/app/src/main/java/it/artform/ExternalProfileActivity.java
+++ artform_android/app/src/main/java/it/artform/ExternalProfileActivity.java
@@ -5,52 +5,88 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import it.artform.pojos.Badge;
+import it.artform.pojos.Post;
+import it.artform.pojos.User;
+import it.artform.web.ArtformApiEndpointInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ExternalProfileActivity extends Activity {
-    ImageView externalProfilePic;
-    TextView usernameExternalProfile;
-    TextView tagsExternalProfile;
-    Button notifyMeExternalProfile;
-    Button badgeExternalProfile;
-    Button contactMe;
+    ImageView externalProfilePicImageView = null;
+    TextView externalProfileUsernameTextView = null;
+    TextView externalUserBioTagsTextView = null;
+    Button nofifyMeButton = null;
+    Button externalProfileBadgeButton = null;
+    Button contactMeButton = null;
+    GridView userPostsGridView = null;
+    RecyclerView badgesReciclerView = null;
+    ArtformApiEndpointInterface apiService = null;
+    Badge[] userBadges = null;
+    Post[] userPosts = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_external_profile);
 
+        externalProfilePicImageView = findViewById(R.id.externalProfilePicImageView);
+        externalProfileUsernameTextView = findViewById(R.id.externalProfileUsernameTextView);
+        externalUserBioTagsTextView = findViewById(R.id.externalUserBioTagsTextView);
+        nofifyMeButton = findViewById(R.id.nofifyMeButton);
+        externalProfileBadgeButton = findViewById(R.id.externalProfileBadgeButton);
+        contactMeButton = findViewById(R.id.contactMeButton);
+        userPostsGridView = findViewById(R.id.userPostsGridView);
 
+        AFGlobal app = (AFGlobal) getApplication();
+        apiService = app.retrofit.create(ArtformApiEndpointInterface.class);
 
-
-        externalProfilePic=findViewById(R.id.externalProfilePicImageView);
-        usernameExternalProfile=findViewById(R.id.usernameExternalProfile);
-        tagsExternalProfile=findViewById(R.id.tagsExternalProfile);
-        badgeExternalProfile=findViewById(R.id.externalProfileBadgeButton);
-
-        contactMe=(Button) findViewById(R.id.contactmeButton);
-        contactMe.setOnClickListener(
-                new View.OnClickListener() {
+        Bundle userParams = getIntent().getExtras();
+        if(userParams != null) {
+            String username = getIntent().getExtras().getString("username");
+            if (username != null) {
+                // carica e imposta i dati del profilo esterno nelle varie View
+                Call<User> getUserCall = apiService.getUserByUsername(username);
+                getUserCall.enqueue(new Callback<User>() {
                     @Override
-                    public void onClick(View view) {
-                        Intent intent=new Intent(ExternalProfileActivity.this, CommissionActivity.class);
-                        startActivity(intent);
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        if (response.isSuccessful()) {
+                            //loadUserData();
+                        }
                     }
-                }
-        ); 
-
-        notifyMeExternalProfile=(Button) findViewById(R.id.nofifyMeExternalProfile);
-        notifyMeExternalProfile.setOnClickListener(
-                new View.OnClickListener() {
                     @Override
-                    public void onClick(View view) {
-                            Toast.makeText(ExternalProfileActivity.this, "Hai attivato le notifiche per l'utente *nome utente* ", Toast.LENGTH_LONG).show();
+                    public void onFailure(Call<User> call, Throwable t) {
+                        t.printStackTrace();
+                        Toast.makeText(ExternalProfileActivity.this, "Error while fetching user data: " + t.toString(), Toast.LENGTH_LONG).show();
                     }
-                }
+                });
+            }
+            else
+                Toast.makeText(this, "Error while fetching user data", Toast.LENGTH_LONG).show();
+        }
 
-        );
+        contactMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent commissionIntent = new Intent(ExternalProfileActivity.this, CommissionActivity.class);
+                startActivity(commissionIntent);
+            }
+        });
 
+        nofifyMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Toast.makeText(ExternalProfileActivity.this, "Hai attivato le notifiche per l'utente *nome utente* ", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
