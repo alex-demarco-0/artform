ba0d600609f0d0ec0d3d6af4dc0086ce5bd7742a
Kendrix29 <94626472+Kendrix29@users.noreply.github.com>
2022-03-07 15:03:59 +0100

    Add ricerca.js

---
+++ ricerca.js
@@ -0,0 +1,17 @@
+/* 
+Data: 07/03/2022
+Descrizione: JavaScript della pagina HTML 'ricerca.html'
+ */
+
+function ricerca() {
+    window.location ='ricerca.html';
+}
+function userProfile(){
+    window.location='userProfile.html';
+}
+function notifiche(){
+    window.location='notifiche.html';
+}
+function homepage(){
+    window.location='homePage.html';
+}
