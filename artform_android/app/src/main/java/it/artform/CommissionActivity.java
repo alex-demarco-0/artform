46f413f181ade1b8024120f9f11b53bbc3d77638
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-03-07 11:49:43 +0100

    CommissionActivity funzionante

--- artform_android/app/src/main/java/it/artform/CommissionActivity.java
+++ artform_android/app/src/main/java/it/artform/CommissionActivity.java
@@ -86,7 +86,7 @@ public class CommissionActivity extends Activity {
                     return;
                 }
                 sendCommissionRequest();
-                //finish();
+                finish();
             }
         });
 
