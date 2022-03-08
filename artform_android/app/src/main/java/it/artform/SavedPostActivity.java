682b63d363258cdc946ae15414727046aa6fbe21
ManbirSinghB <s.manbir431@gmail.com>
2022-03-08 15:17:25 +0100

    Creazione Activity per i post salvati

---
+++ artform_android/app/src/main/java/it/artform/SavedPostActivity.java
@@ -0,0 +1,11 @@
+package it.artform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SavedPostActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
