575af28ec2d59b517d378a723b482f99e67e8e33
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-03-08 15:13:06 +0100

    impostata PostDetailsActivity

---
+++ artform_android/app/src/main/java/it/artform/PostDetailsActivity.java
@@ -0,0 +1,14 @@
+package it.artform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.row_post_list);
+
+    }
+}
