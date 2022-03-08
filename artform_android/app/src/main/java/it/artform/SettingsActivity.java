0a7172c72df3ac16b19048950d93ae2a12522c18
ManbirSinghB <s.manbir431@gmail.com>
2022-03-08 12:50:14 +0100

    modificato listView e aggiunto switch/case

--- artform_android/app/src/main/java/it/artform/SettingsActivity.java
+++ artform_android/app/src/main/java/it/artform/SettingsActivity.java
@@ -27,10 +27,9 @@ public class SettingsActivity extends Activity {
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Saved post");
         arrayList.add("Account");
+        arrayList.add("FAQ & Help");
         arrayList.add("Privacy");
-        arrayList.add("Sicurezza");
-        arrayList.add("Assistenza");
-        arrayList.add("Informazioni");
+        arrayList.add("Termini e Condizioni");
         arrayList.add("Logout");
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
@@ -40,36 +39,24 @@ public class SettingsActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-
+                // MIGLIORATO CON FRAGMENT.
                 switch (i){
-                    case 0:
-
+                    case 0: // post salvati
                         break;
-                    case 1:
+                    case 1: // Account -- cambio username, numero di telefono, passeword - gestione notifiche e varie impostazioni
                         Intent accountIntent = new Intent(SettingsActivity.this, SettingsAccountActivity.class);
                         startActivity(accountIntent);
                         break;
-                    case 6:
+                    case 2: // FAQ -- bug report
+                        openGmail();
                         break;
-                }
-
-                // logout
-                if (i == 6) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
-                    builder.setTitle("Confirmation PopUp!").
-                            setMessage("You sure, that you want to logout?");
-                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            Toast.makeText(SettingsActivity.this, "AHHHHHHHh", Toast.LENGTH_LONG).show();
-                        }
-                    });
-                    builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.cancel();
-                        }
-                    });
-                    AlertDialog alert11 = builder.create();
-                    alert11.show();
+                    case 3: // Privacy
+                        break;
+                    case 4: // termini e condizioni
+                        break;
+                    case 5: // logout
+                        logout();
+                        break;
                 }
             }
         });
@@ -83,6 +70,36 @@ public class SettingsActivity extends Activity {
             }
         });
     */
+    }
+
+    private void openGmail() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        String[] emails_in_to={"to@email.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL, emails_in_to );
+        intent.putExtra(Intent.EXTRA_SUBJECT,"Your Email's subject");
+        intent.putExtra(Intent.EXTRA_TEXT, "Your Email's predefined Body");
+        intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+        intent.setType("text/html");
+        intent.setPackage("com.google.android.gm");
+        startActivity(intent);
+    }
+
+    private void logout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+        builder.setTitle("Confirmation PopUp!").
+                setMessage("You sure, that you want to logout?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(SettingsActivity.this, "AHHHHHHHh", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert11 = builder.create();
+        alert11.show();
     }
 }
 
