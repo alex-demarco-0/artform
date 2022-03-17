4334d5f2b885c55f789038db4bb88ca16b54b9ce
Alessandro De Marco <a.demarco@itsrizzoli.it>
2022-03-17 15:57:52 +0100

    bugfix

--- artform_springboot/src/main/resources/static/webpages/userProfile.js
+++ artform_springboot/src/main/resources/static/webpages/userProfile.js
@@ -4,13 +4,14 @@ var userObj = null;
 function getUser() {
     var userXmlHttp = new XMLHttpRequest();
     userXmlHttp.onreadystatechange = function() {
-        if(userXmlHttp.readyState == 4 && userXmlHttp.status == 200) {
-            userObj = JSON.parse(userXmlHttp.responseText);
-            document.getElementById("propic").setAttribute("src", base_url + "media/userProfilePics/" + userObj.username + ".jpg");
-            document.getElementById("userUsername").innerHTML = userObj.username;
-            document.getElementById("userBio").innerHTML = userObj.bio;
-            getUserPosts();
-        }
+        if(userXmlHttp.readyState == 4) 
+            if(userXmlHttp.status == 200) {
+                userObj = JSON.parse(userXmlHttp.responseText);
+                document.getElementById("propic").setAttribute("src", base_url + "media/userProfilePics/" + userObj.username + ".jpg");
+                document.getElementById("userUsername").innerHTML = userObj.username;
+                document.getElementById("userBio").innerHTML = userObj.bio;
+                getUserPosts();
+            }
     };
     var userLoginSesson = window.localStorage;
     var userUri = base_url + "artform/utente/" + userLoginSesson.getItem("username"); // utente loggato
@@ -21,14 +22,15 @@ function getUser() {
 function getUserPosts() {
     var postsXmlHttp = new XMLHttpRequest();
     postsXmlHttp.onreadystatechange = function() {
-        if(postsXmlHttp.readyState == 4 && postsXmlHttp.status == 200) {
-            var postListObj = JSON.parse(postsXmlHttp.responseText);
-            for(var idx=0; idx<postListObj.length; idx++) {
-                var post = postListObj[idx];
-                document.getElementById("posts").innerHTML +=
-                '<img src="' + base_url + 'media/imagePosts/' + post.id + '.jpg">'
+        if(postsXmlHttp.readyState == 4)
+            if(postsXmlHttp.status == 200) {
+                var postListObj = JSON.parse(postsXmlHttp.responseText);
+                for(var idx=0; idx<postListObj.length; idx++) {
+                    var post = postListObj[idx];
+                    document.getElementById("posts").innerHTML +=
+                    '<img src="' + base_url + 'media/imagePosts/' + post.id + '.jpg">'
+                }
             }
-        }
     };
     postsXmlHttp.open("GET", base_url + "artform/utente/" + userObj.username + "/posts");
     postsXmlHttp.send(null);
