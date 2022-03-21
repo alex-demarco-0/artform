a9a38a9c97050522d8bf991421437fca2076bafd
Kendrix29 <94626472+Kendrix29@users.noreply.github.com>
2022-03-21 12:33:47 +0100

    Add File commissioni.js
    funzione della pubblicazione della commissione

---
+++ artform_springboot/src/main/resources/static/webpages/commissioni.js
@@ -0,0 +1,46 @@
+var base_url = "http://localhost:8080/";
+var formData = null;
+
+function pubblicaCommissione() {
+    var titolo = document.getElementById("inputTitle").value;
+    var offerta = document.getElementById("money").value;
+    var topic= document.getElementsById("topicSelect").value;
+    var dataScadenza= document.getElementById("date").value;
+  
+    if(titolo == "") {
+      window.alert("Inserisci un titolo");
+      return;
+    }
+    else if (offerta == "") {
+      window.alert("Seleziona un' offerta");
+      return;
+    }
+    else if(topic==""){
+        window.alert("Inserisci un Topic");
+    }
+    else if(dataScadenza==""){
+        window.alert("Inserisci una data di scadenza");
+    }
+  
+    var artist = window.localStorage.getItem("username");
+    var commisioneJsonData = '{"artistaUsername":"' + artist + 
+                  '","titolo":"' + titolo +
+                  '","offerta":"' + offerta +
+                  '","topic":"' + topic + 
+                  '","data scadenza":"' + dataScadenza +
+                  '"}';
+  
+    var addCommissioneXmlHttp = new XMLHttpRequest();
+    addPostXmlHttp.onreadystatechange = function() {
+          if(addCommissioneXmlHttp.readyState == 4) {
+              if(addCommissioneXmlHttp.status == 201) {
+                  window.alert("Pubblicazione avvenuta con successo!");
+              }
+              else
+                  window.alert("Si è verificato un problema durante la pubblicazione della commissione");
+          }
+      };
+      addCommissioneXmlHttp.open("POST", base_url + "artform/commisioni");
+      addCommisioneXmlHttp.setRequestHeader("Content-Type", "application/json");
+      addCommissioneXmlHttp.send(commissionitJsonData);
+  }
