6d1bf7b075da8224570b91cc3792b9ebf141ec8d
Kendrix29 <94626472+Kendrix29@users.noreply.github.com>
2022-05-10 12:20:51 +0200

    Update addPost.js

--- artform_springboot/src/main/resources/static/webpages/addPost.js
+++ artform_springboot/src/main/resources/static/webpages/addPost.js
@@ -23,6 +23,17 @@ function pubblica() {
     window.alert("Seleziona un file");
     return;
   }
+   
+  
+  // Reindirizza la pagina del browser non supportata se aperta da un browser che non supporta le funzioni asincrone
+            try {
+                eval("async () => {}");
+            } catch (error) {
+                var homeUrl = window.location.origin + window.location.pathname;
+                var appUrl = homeUrl.slice(0, homeUrl.lastIndexOf("/") + 1);
+                window.location.replace(appUrl + "unsupported-browser.html");
+            }
+        
     
   if (!document.cookie || !document.cookie.match(/(^|;)originURI=/gi))
   document.cookie = "originURI=/login.html" + (window.location.protocol === "https:" ? ";SameSite=None;Secure" : "");
